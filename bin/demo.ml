99cca160acb76b30f0ae3db97f7a4127fa57d448
Michel Perez <michel.ingesoft@gmail.com>
2017-11-03 21:54:02 -0500

    wip

--- bin/demo.ml
+++ bin/demo.ml
@@ -1,3 +1,11 @@
-let () =
+let say_hello name =
   let name = "michel" in
-  print_string ("Hola mundo " ^ name)
+  print_string ("Hola mundo " ^ name ^ "\n")
+
+let () =
+  for i = 0 to 10 do
+    say_hello ();
+  done;
+  Core.List.iter (Core.List.range 1 10) (fun elem ->
+      print_string (string_of_int elem));
+  ()
