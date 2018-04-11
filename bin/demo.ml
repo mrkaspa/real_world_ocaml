635c7593f4990860425debeb60e8bcb1cbb7fe45
Michel Perez <michel.ingesoft@gmail.com>
2018-04-11 14:10:33 -0500

    adding lwt experiment

--- bin/demo.ml
+++ bin/demo.ml
@@ -1,8 +1,35 @@
 open Core
+open Lwt
 
 let say_hello name =
   let name = "michel" in
   print_string ("Hola mundo " ^ name ^ "\n")
+
+let lwt_process () =
+  let request =
+    let%lwt addresses = Lwt_unix.getaddrinfo "google.com" "80" [] in
+    let address = List.hd addresses in
+    match address with
+    | Some address ->
+      let google = Lwt_unix.(address.ai_addr) in
+
+      Lwt_io.(with_connection google (fun (incoming, outgoing) ->
+        let%lwt () = write outgoing "GET / HTTP/1.1\r\n" in
+        let%lwt () = write outgoing "Connection: close\r\n\r\n" in
+        let%lwt response = read incoming in
+        Lwt.return (Some response)))
+    | None ->
+      Lwt.return None
+  in
+
+  let timeout =
+    let%lwt () = Lwt_unix.sleep 5. in
+    Lwt.return None
+  in
+
+  match Lwt_main.run (Lwt.pick [request; timeout]) with
+  | Some response -> print_string response
+  | None -> prerr_endline "Request timed out"; exit 1
 
 let () =
   for i = 0 to 10 do
@@ -10,4 +37,4 @@ let () =
   done;
   List.iter (List.range 1 10) (fun elem ->
       print_string (string_of_int elem));
-  ()
+  lwt_process ()
