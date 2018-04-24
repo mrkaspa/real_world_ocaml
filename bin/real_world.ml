fef9c4537ccf692f05e77370f88b5bd6a88c4f15
Michel Perez <michel.ingesoft@gmail.com>
2018-04-24 16:13:21 -0500

    wip

--- bin/real_world.ml
+++ bin/real_world.ml
@@ -4,5 +4,5 @@ let () =
   print_string ("ZERO = " ^ zero ^ "\n");
   let res = Lib.Ls_assoc.find_in_map () in
   match res with
-  |Some x -> print_string x
-  |None -> print_string "EEEE"
+  | Some x -> print_string x
+  | None -> print_string "EEEE"
