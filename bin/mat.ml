a1a5ad948a8c3f1a30c4120b298312d27bb6f250
Michel Perez <michel.ingesoft@gmail.com>
2018-04-26 10:28:14 -0500

    wip

---
+++ bin/mat.ml
@@ -0,0 +1,13 @@
+let create_matrix n =
+  Array.make_matrix n n 0
+
+let run () =
+  let mat = create_matrix 1000 in
+  Array.fold_left
+    (fun acc arr -> Array.fold_left (+) acc arr) 0 mat
+
+let () =
+  let t = Sys.time() in
+  run () |> ignore;
+  let delta = Sys.time() -. t in
+  Printf.printf "Execution time: %fms\n" (delta *. 1000.0)
