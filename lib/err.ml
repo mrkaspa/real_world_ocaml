88023c652b93d3bdd76529a641b9a4881c13c0e4
Michel Perez <michel.ingesoft@gmail.com>
2018-06-13 19:38:57 -0500

    wip

--- lib/err.ml
+++ lib/err.ml
@@ -1,12 +1,11 @@
 open Core
 
+exception DemoEx of string
+
 let safe_div n d =
   if d = 0 then Error (Error.of_string "d is 0") else Ok (n / d)
 
-exception DemoEx of string
-
-let unsafe_div n d =
-  if d = 0 then raise (DemoEx "d is 0") else n / d
+let unsafe_div n d = if d = 0 then raise (DemoEx "d is 0") else n / d
 
 let use_safe_div () =
   let open Result.Monad_infix in
@@ -15,12 +14,10 @@ let use_safe_div () =
   | Ok res -> printf "The res %d" res
   | Error error -> printf "Err %s" (Error.to_string_hum error)
 
-
 let use_unsafe_div () =
   try
     let res = unsafe_div 1 0 in
     printf "The res %d" res
   with DemoEx msg -> printf "Err %s" msg
-
 
 let () = use_unsafe_div ()
