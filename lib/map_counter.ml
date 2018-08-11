cea82e2386a35d72d3fddccf60c306341f4cface
Michel Perez <michel.ingesoft@gmail.com>
2018-08-11 14:48:05 -0500

    wip

--- lib/map_counter.ml
+++ lib/map_counter.ml
@@ -9,3 +9,9 @@ let to_list t = Map.to_alist t
 let touch t s =
   let count = match Map.find t s with None -> 0 | Some x -> x in
   Map.set t ~key:s ~data:(count + 1)
+
+let make_int_map ls = Map.of_alist_exn (module Int) ls
+
+let run () =
+  let m = make_int_map [1, ""; 2, ""] in
+  Map.find m 1
